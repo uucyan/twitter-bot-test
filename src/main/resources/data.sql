87df98062c4a4ca44365c5b853c5f1467ff16c4b
uucyan <uuunouucyan@gmail.com>
2018-03-18 14:26:06 +0900

    Consumer系の情報を更新

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,5 @@ delete from tweet_log;
 delete from twitter_application;
 insert into twitter_application VALUES
   (1, 'dev', '@dev_bot_cyan', 'https://twitter.com/dev_bot_cyan', 'かいはつぼっとちゃん', 'DRrJp1VWExASN64BVgknupI3T', 'NgWxrk0cWk1VJVbYV0SQFmVk8ahdzKSqphxfGsHbIdutEBiMgp', '938316768862527488-maea5I06YkBkRBbmHdoUIHUmV0zNb9c', 'jyoxx4WX82OFxmQeRIv46NjHZTxEM0OBnBnEFE2mjdt9d', true, NOW() , NOW()),
-  (2, 'prod', '@prod_bot_cyan', 'https://twitter.com/prod_bot_cyan', 'ほんばんぼっとちゃん', '6HdkRW8TLZIE8GfiSUPsXyfiJ', 'IU4XSpqxonp1KdTnOUwuBNJlZkyr2gZd8Y6e3MkDw7n3QPSZS4', '947152326552313856-Qdwh3YS907tY4sYHPGbMwXyWTMN8R18', 'oDpEW5Oa9Mb0bmcLtSgFGOGxVgXf4leJf6mrO6amsJyUE', true, NOW() , NOW())
+  (2, 'prod', '@prod_bot_cyan', 'https://twitter.com/prod_bot_cyan', 'ほんばんぼっとちゃん', 'awkheaBr6XCiadpeJZMLttBkf', 'UfvVPsmZDFDHUaq8Jb3TEaPytG6XNIYfLLKQOfSNd877h5R6jg', '947152326552313856-Qdwh3YS907tY4sYHPGbMwXyWTMN8R18', 'oDpEW5Oa9Mb0bmcLtSgFGOGxVgXf4leJf6mrO6amsJyUE', true, NOW() , NOW())
 ;
